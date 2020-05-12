bdfe38af2521aa23dcab8934ee659e5c0ec7afc1
jad el mir <jadelmir>
2020-05-12 18:46:22 -0400

    Final

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["estonia","france","germany","ireland","italy","nigeria","poland","russia","spain","uk","us"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var ShowingScore = false
+    @State private var scoreTitle = ""
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing:30){
+                  
+                  VStack{
+                            Text("Tap the flag of")
+                                .foregroundColor(.white)
+                            Text(countries[correctAnswer])
+                                .foregroundColor(.white)
+                        }
+                        ForEach(0 ..< 3 ){number in
+                          Button(action: {
+                            self.flagTapped(number)
+                          }){
+                                    Image(self.countries[number]).renderingMode(.original)
+                          }
+                        }
+                Spacer()
+              }
+            
+        }
+        .alert(isPresented: $ShowingScore){
+            Alert(title: Text("hello"), message: Text("\(scoreTitle)"), dismissButton: .default(Text("Continue")){
+                self.askQuestion()
+                })
+        }
+    }
+    
+    func askQuestion(){
+        countries.shuffled()
+        correctAnswer = Int.random(in: 0 ..< 3 )
+    }
+    func flagTapped(_ number:Int){
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        }else {
+            scoreTitle = "Wrong"
+        }
     }
 }
 
